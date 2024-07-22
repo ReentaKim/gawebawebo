08b1057de5afb3b1864327a1aab520894e89920d
Reenta <reenta.kim@gmail.com>
2024-07-22 15:47:52 +0900

    complete!!

--- src/App.js
+++ src/App.js
@@ -1,147 +1,68 @@
-import React, {useState} from 'react';
 import './App.css';
-import rockImage from './images/rock.jpg';
-import paperImage from './images/paper.jpg';
-import scissorsImage from './images/scissors.jpg';
 import Box from "./component/Box";
+import {useState} from "react";
 
 function App() {
-    const [gameMode, setGameMode] = useState(true);
-    const [userChoice, setUserChoice] = useState(null);
-    const [computerChoice, setComputerChoice] = useState(null);
-    const [result, setResult] = useState('');
 
-    const choices = [
-        {name: '가위', image: scissorsImage},
-        {name: '바위', image: rockImage},
-        {name: '보', image: paperImage}
-    ];
+    const [userChoice, setUserChoice] = useState(null);
+    const [computerCoice, setComputerCoice] = useState(null);
+    const [result, setResult] = useState('');
 
     const choice = {
         rock: {
-            name: 'rock',
-            img: rockImage,
+            name: '묵',
+            img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9nimNy57DDFxHsy8OFyFnfEXLSig2_ngkgw&s'
         },
-        scissors: {
-            name: 'scissors',
-            img: scissorsImage,
+        scissor: {
+            name: '찌',
+            img: 'https://m.chenjo.co.kr/web/product/big/202201/9395bdb9b1e17c44d6bf400e3675e0a6.jpg'
         },
         paper: {
-            name: 'paper',
-            img: paperImage,
+            name: '빠',
+            img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRclpb0c1fCMNorcrcyMVNG3DEv_-OSxsx-PwbI4tPNc7pPn1HSCbTitMFt2s0EvVwD3tA&usqp=CAU'
         }
-    };
-
-
-    const handleClick = (choice) => {
-        setUserChoice(choice);
-        const randomChoice = choices[Math.floor(Math.random() * choices.length)];
-        setComputerChoice(randomChoice);
-        determineWinner(choice.name, randomChoice.name);
-    };
-
-    const determineWinner = (user, computer) => {
-        if (user === computer) {
-            setResult('비겼습니다');
-        } else if (
-            (user === '가위' && computer === '보') ||
-            (user === '바위' && computer === '가위') ||
-            (user === '보' && computer === '바위')
-        ) {
-            setResult('이겼습니다');
-        } else {
-            setResult('졌습니다');
-        }
-    };
-
-    const getResultColor = () => {
-        if (result === '이겼습니다') return 'green';
-        if (result === '졌습니다') return 'red';
-        return 'black';
-    };
+    }
 
     const play = (userChoice) => {
-        // console.log('선택됨', userChoice);
         setUserChoice(choice[userChoice]);
-
-        let computerChoice = randomChoice();
-        setComputerChoice(computerChoice);
-        judgment(choice[userChoice], computerChoice);
+        const computer = choice[randomComputer()];
+        setComputerCoice(computer);
+        calculate(choice[userChoice], computer);
     }
 
-    const randomChoice = () => {
-        let itemArray = Object.keys(choice);
-        // console.log('Item array', itemArray);
-        let randomItem = Math.floor(Math.random() * itemArray.length);
-        let finals = itemArray[randomItem];
-
-        return choice[finals];
-    }
-
-    const judgment = (user, computer) => {
-        console.log('user', user, 'computer', computer);
+    const calculate = (user, computer) => {
+        console.log(user)
+        console.log(computer)
         if (user.name === computer.name) {
-            setResult('비겼습니다');
-        } else if (
-            (user.name === 'scissors' && computer.name === 'paper') ||
-            (user.name === 'rock' && computer.name === 'scissors') ||
-            (user.name === 'paper' && computer.name === 'rock')
+            setResult('비겼습니다.');
+        } else if ((user.name === '묵' && computer.name === '찌') ||
+            (user.name === '찌' && computer.name === '빠') ||
+            (user.name === '빠' && computer.name === '묵')
         ) {
-            setResult('이겼습니다');
+            setResult('이겼습니다.');
         } else {
-            setResult('졌습니다');
+            setResult('졌습니다.');
         }
+
     }
 
+    const randomComputer = () => {
+        const keys = Object.keys(choice);
+        const result = Math.floor(Math.random() * keys.length);
+        return keys[result];
+    }
 
     return (
-        <div className="app">
-            <button onClick={() => setGameMode(!gameMode)}>변경</button>
-            {gameMode === true ?
-                <div>
-                    <div className={'main'}>
-                        <Box title={'You'} item={userChoice} result={result}/>
-                        <Box title={'Computer'} item={computerChoice} result={result}/>
-                    </div>
-                    <div className={'main'}>
-                        <button onClick={() => play('scissors')}>가위</button>
-                        <button onClick={() => play('rock')}>바위</button>
-                        <button onClick={() => play('paper')}>보</button>
-                    </div>
-                </div>
-                :
-                <div className="game-box">
-                    <h1>가위바위보 게임</h1>
-                    <div className="choices">
-                        <div className="choice user-choice">
-                            <h2>나</h2>
-                            {userChoice && <img src={userChoice.image} alt={userChoice.name} width={'200px'}/>}
-                            <p>{userChoice?.name}</p>
-                        </div>
-                        <div className="choice computer-choice">
-                            <h2>컴퓨터</h2>
-                            {computerChoice &&
-                                <img src={computerChoice.image} alt={computerChoice.name} width={'200px'}/>}
-                            <p>{computerChoice?.name}</p>
-                        </div>
-                    </div>
-                    <div className="result" style={{borderColor: getResultColor()}}>
-                        <h2>{result}</h2>
-                    </div>
-                    <div className="buttons">
-                        {choices.map((choice) => (
-                            <img
-                                key={choice.name}
-                                src={choice.image}
-                                alt={choice.name}
-                                onClick={() => handleClick(choice)}
-                                className="choice-image"
-                                width={'200px'}
-                            />
-                        ))}
-                    </div>
-                </div>
-            }
+        <div>
+            <div className={'main'}>
+                <Box title={'You'} result={result} item={userChoice}/>
+                <Box title={'Computer'} result={result} item={computerCoice}/>
+            </div>
+            <div className={'main'}>
+                <button onClick={() => play('scissor')}>묵</button>
+                <button onClick={() => play('rock')}>찌</button>
+                <button onClick={() => play('paper')}>빠</button>
+            </div>
         </div>
     );
 }
--- src/component/Box.js
+++ src/component/Box.js
@@ -1,11 +1,27 @@
 import React from 'react';
 
 const Box = (props) => {
+    let result;
+    let resultColor;
+    if (props.result !== '') {
+        if (props.title === 'Computer' && props.result !== '비겼습니다.' && props.result !== '') {
+            result = props.result === '이겼습니다.' ? '졌습니다.' : '이겼습니다.';
+            resultColor = props.result === '이겼습니다.' ? 'lose' : 'win';
+        } else {
+            result = props.result;
+            resultColor = props.result === '이겼습니다.' ? 'win' : props.result === '졌습니다.' ? 'lose' : 'tie';
+        }
+    } else {
+        result = '';
+        resultColor = '';
+    }
     return (
-        <div className="box">
+        <div className={`box ${resultColor}`}>
             <h1>{props.title}</h1>
-            <img className={'item-img'} src={props.item && props.item.img} alt="demo"/>
-            <h2>{props.result}</h2>
+            <img className={'item-img'}
+                 src={props.item && props.item.img}
+                 alt=""/>
+            <h2>{result}</h2>
         </div>
     );
 };
